121a3b7937dc37c316824229eef157170c25857c
Benjamin <benteezy7@gmail.com>
2025-02-28 12:29:44 +0100

    setting up server

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+require('dotenv').config();
+
+
+
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+// Middleware
+
+
+// Routes
+
+
+
+
+
+
+module.exports = app;
